67de24f1ce74bf45a50a07aa61fd308579d90c2b
Toby PC <tobymoore1995@gmail.com>
2021-08-07 20:03:35 +0100

    Addition to previous commit.

--- src/pages/components/imagegrid/imagegrid.js
+++ src/pages/components/imagegrid/imagegrid.js
@@ -1,7 +1,24 @@
 import "./imagegrid.css";
 
 function ImageGrid() {
-  return <div className="image-grid-screen">Image Grid</div>;
+  return (
+    <div className="image-grid-screen">
+      <div className="image-grid-wrapper">
+        <div className="image-grid-content">
+          <div className="demo-image">Image</div>
+          <div className="demo-image">Image</div>
+          <div className="demo-image">Image</div>
+          <div className="demo-image">Image</div>
+          <div className="demo-image">Image</div>
+          <div className="demo-image">Image</div>
+          <div className="demo-image">Image</div>
+          <div className="demo-image">Image</div>
+          <div className="demo-image">Image</div>
+          <div className="demo-image">Image</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ImageGrid;
