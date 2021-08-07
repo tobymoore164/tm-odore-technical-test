35ba7e2c2c1d0b3a7f7c0a432dff88572f913885
Toby PC <tobymoore1995@gmail.com>
2021-08-07 19:30:30 +0100

    Created header with responsive styling.

--- src/pages/components/header/header.js
+++ src/pages/components/header/header.js
@@ -1,7 +1,39 @@
+import { useEffect, useState } from "react";
 import "./header.css";
 
 function Header() {
-  return <div className="header-wrapper">Header</div>;
+  const [mobileView, setMobileView] = useState(false);
+  // Window Resize Event Listener for Mobile Topbar
+  useEffect(() => {
+    const setResponsiveness = () => {
+      return window.innerWidth < 900
+        ? setMobileView(true)
+        : setMobileView(false);
+    };
+
+    setResponsiveness();
+    window.addEventListener("resize", () => setResponsiveness());
+
+    return () => {
+      window.removeEventListener("resize", () => setResponsiveness());
+    };
+  }, []);
+
+  return (
+    <div className="header-screen">
+      {/* Need a nested div so the parent can have grey background otherwise corners will have blank spots */}
+      <div
+        className={`header-wrapper ${
+          mobileView ? "mobile-header-wrapper" : ""
+        }`}
+      >
+        <div className="header-title BOLD">Simple Images.</div>
+        <div className="header-author">
+          by <span className="BOLD">Toby Moore</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
